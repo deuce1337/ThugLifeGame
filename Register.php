32dcfc9256f3992183029f6b477fface50a5fd99
Jakub Wierzchowski <Kuba@MacBook-Jakub.home>
2016-04-20 22:16:57 +0200

    Initial commit

---
+++ Register.php
@@ -0,0 +1,16 @@
+<?php
+    $con = mysqli_connect("mysql6.000webhost.com", "a2003414_root", "thuglife34", "a2003414_users");
+    
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $statement = mysqli_prepare($con, "INSERT INTO players (name, age, username, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
---
+++ app/src/main/java/com/example/kamel/thuglifegame/GameActivity.java
@@ -0,0 +1,35 @@
+package com.example.kamel.thuglifegame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        final TextView Username = (TextView) findViewById(R.id.tvUsername);
+
+        final ImageButton ibBank = (ImageButton) findViewById(R.id.ibBank);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+
+        Username.setText(username);
+
+        ibBank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bankIntent = new Intent(GameActivity.this, BankActivity.class);
+                GameActivity.this.startActivity(bankIntent);
+            }
+        });
+
+    }
+}
