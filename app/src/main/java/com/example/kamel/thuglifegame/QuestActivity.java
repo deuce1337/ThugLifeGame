5a6d3b41afc1b5c9257554abce8c7cd6268ccfc7
Kamel <kamel345@gmail.com>
2016-04-24 15:05:17 +0200

    Działa logowanie na hostingerze !!

--- app/src/main/java/com/example/kamel/thuglifegame/QuestActivity.java
+++ app/src/main/java/com/example/kamel/thuglifegame/QuestActivity.java
@@ -1,19 +1,45 @@
 package com.example.kamel.thuglifegame;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class QuestActivity extends AppCompatActivity {
+
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest);
 
+
+        SQLiteDatabase mydatabase = openOrCreateDatabase("db_list",MODE_PRIVATE,null);
+
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS questsList(name VARCHAR,description VARCHAR);");
+        mydatabase.execSQL("INSERT INTO questsList VALUES('Staruszka','Ukradnij portfel babci');");
+
         final ListView questList = (ListView) findViewById(R.id.lvQuest);
 
-//Tu bede pracował Ja (Kamil)
+        Cursor resultSet = mydatabase.rawQuery("Select * from questsList",null);
+        resultSet.moveToFirst();
+        String name = resultSet.getString(1);
+        String description = resultSet.getString(2);
+
+
+        arrayList = new ArrayList<String>();
+        arrayList.add(name);
+        arrayList.add(description);
+
+        //adapter = new ArrayAdapter<String>(this,arrayList);
+
 
     }
 }
